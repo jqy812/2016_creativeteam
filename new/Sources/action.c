f35203fb9fb74ba186f0258af51fe92be17d02ae
jqy812 <812jqy@tongji.edu.cn>
2016-08-15 22:48:29 +0800

    4号车程序

--- new/Sources/action.c
+++ new/Sources/action.c
@@ -106,7 +106,7 @@ void RFID_control_car_3_action(DWORD site)
 /*-----------------------------------------------------------------------*/
 void RFID_control_car_4_action(DWORD site)
 {
-	if((site>>8)!=0x25 && (site>>8)!=0x23)
+	if((site>>8)!=0x25 && (site>>8)!=0x23 && (site>>8)!=0x0A)
 		sending_service_package(0x33,0x00CD,site);
 	if((site>>8)==0x21 && (site>>0)!=0x2103)//在左打死路口         并起道路动作切换功能，日后需更改               jqy
 	{
@@ -120,28 +120,31 @@ void RFID_control_car_4_action(DWORD site)
 	}
 	if((site>>0)==0x0A01 && bz==4)
 	{
-		RunL=1;		
+		delay_count=0;	
+		RunL=1;	
 		g_f_enable_speed_control=0;
 		set_speed_pwm(0);
 //		velocity=-9;
 //		set_speed_target(-9);
 //		fieldover=1;
 //		bz=6;
-    	delay_ms(3000);
+    	delay_ms(1000);
+		sending_service_package(0x33,0x00CD,0x0A01);
     	set_steer_helm_basement(data_steer_helm_basement.center);
     	g_f_enable_speed_control=1;
-		delay_count=0;	
 		set_speed_target(-9);
-		while(delay_count>-3900)
+		while(delay_count>-3300)
 		{
 			LCD_Write_Num(105,3,delay_count,4);
 		}	
 		g_f_enable_speed_control=0;
 		set_speed_pwm(0);
 		bz=6;
+		sending_service_package(0x33,0x00CD,0x0A02);
 		delay_ms(1000);
 		Hang_Up();
 		delay_ms(1000);
+		sending_service_package(0x33,0x00CD,0x0A03);
 		bz=-6;
 		fieldover=1;
 		g_f_enable_speed_control=1;
@@ -159,6 +162,7 @@ void RFID_control_car_4_action(DWORD site)
 //	}
 	if((site>>0)==0x8004)
 	{
+		set_speed_target(13);
 		delay_count=0;
 		set_steer_helm_basement(data_steer_helm_basement.left_limit);
 		while(delay_count<2700)
@@ -167,21 +171,14 @@ void RFID_control_car_4_action(DWORD site)
 		}
 		delay_count=0;
 		set_steer_helm_basement(data_steer_helm_basement.center);
-		while(delay_count<550)
-		{
-			LCD_Write_Num(105,3,delay_count,4);
-		}
-		set_speed_target(0);
-		EMIOS_0.CH[10].CBDR.R = 2000;
-		RunL=0;
-		delay_ms(2000);
-		delay_count=0;
-		while(delay_count<500)
+		while(delay_count<1100)
 		{
 			LCD_Write_Num(105,3,delay_count,4);
 		}
 		g_f_enable_speed_control=0;
 		set_speed_pwm(0);
+		EMIOS_0.CH[10].CBDR.R = 2000;
+		RunL=0;
 		Car_Waitfororder=1;
 	}
 	if((site>>0)==0x8009)
@@ -200,16 +197,17 @@ void RFID_control_car_4_action(DWORD site)
 		}
 		delay_count=0;
 		set_steer_helm_basement(data_steer_helm_basement.center);
-		while(delay_count<700)
+		while(delay_count<1000)
 		{
 			LCD_Write_Num(105,3,delay_count,4);
 		}
 		delay_count=0;
 		set_steer_helm_basement(data_steer_helm_basement.right_limit);
-		while(delay_count<400)
+		while(delay_count<300)
 		{
 			LCD_Write_Num(105,3,delay_count,4);
 		}
+		sending_service_package(0x33,0x00CD,0x0A04);
 		bz=4;
 	}
 }
@@ -282,63 +280,9 @@ void device_Num_change(void)//把设备号换成16进制（好像没啥用）
 }
 void car_default()//每次开车将宝马所有标志位置为默认状态，防止起冲突
 {
-	if(WIFI_ADDRESS_CAR_1 == g_device_NO)
-	{
-		have_responsed=1;
-		waiting_for_response=0;
-	}
-	if(WIFI_ADDRESS_CAR_3 == g_device_NO)
-	{
-		if(Door_Close_Run)
-			Door_Close_Run=0;
-		if(Door_Delay)
-			Door_Delay=0;
-		set_door_pwm(0);
-		if(Car_Psg)
-			Car_Psg=0;
-		if(Door_Open)
-			Door_Open=0;
-		if(Door_Close)
-			Door_Close=0;
-		if(Door_Status)
-			Door_Status=0;
-		if(Door_Stop)
-			Door_Stop=0;
-		if(DoorC!=0)
-			DoorC=0;
-	}
 }
 void BMW_Taxi()//宝马开关门开车控制
 {
-	if(Door_Open)
-	{
-		Door_Status=1;	
-		set_door_pwm(600);
-		Door_Open=0;
-	}
-	if(Door_Close)
-	{
-		Door_Status=1;
-		set_door_pwm(-600);
-		Door_Close=0;
-	}
-	if(Door_Stop)
-	{
-		set_door_pwm(0);
-		Door_Stop=0;
-		if(Door_Delay)
-		{
-			Car_Stop=0;
-			Door_Delay=0;
-		}
-	}
-	if(Door_Close_Run)
-	{
-		Door_Status=1;
-		set_door_pwm(-600);
-		Door_Close_Run=0;
-		Door_Delay=1;
-	}
 }
 
 void zhangai_run()
@@ -403,11 +347,6 @@ void Start_one()
 				g_remote_frame_state = REMOTE_FRAME_STATE_NOK;
 				Wifi_Ctrl();
 			}
-//			if(order_received==1)
-//			{
-//				order_received=0;
-//				generate_remote_frame_2(g_device_NO_Hex, 0x33, 0x0000, 2, (const BYTE *)(&response_data));
-//			}
 		}
 		velocity=16;
 		set_speed_target(velocity);
@@ -419,7 +358,7 @@ void Start_one()
 		}
 		set_steer_helm_basement(data_steer_helm_basement.left_limit);
 		delay_count=0;
-		while(delay_count<4500)
+		while(delay_count<5000)
 		{
 			LCD_Write_Num(105,3,delay_count,4);
 		}
